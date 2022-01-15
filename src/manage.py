90a878c04bfa2fc627f2aaf754ac5df55dd174e0
Rick Henry <rickhenry@rickhenry.dev>
2022-01-14 22:41:52 -0500

    re-add manage script for creating apps from the command line
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/manage.py
+++ src/manage.py
@@ -1,23 +1,13 @@
+import asyncio
 import uuid
 from typing import Optional
 from urllib.parse import quote_plus
 import jwcrypto.jwk as jwk
 
-import pymongo
 import click
 
 from app import config
 from app.models.client_app_model import ClientApp
-
-db_uri = "mongodb://{username}:{password}@{host}:{port}".format(
-    username=quote_plus(config.DB_USERNAME),
-    password=quote_plus(config.DB_PASSWORD),
-    host=quote_plus(config.DB_HOST),
-    port=quote_plus(config.DB_PORT),
-)
-
-db_client = pymongo.MongoClient(db_uri)
-db = db_client[config.DB_NAME]
 
 
 @click.group()
@@ -47,12 +37,13 @@ def createapp(
         refresh_key=None,
         refresh_token_expire_hours=None,
         redirect_url=url,
+        owner=config.WEBMASTER_EMAIL,
     )
     if refresh:
         app.set_refresh_key(jwk.JWK.generate(kty="EC", size=4096))
         app.refresh_token_expire_hours = refresh_token_expire_hours or 24
     app.set_key(key)
-    db.client_app.insert_one(app.dict())
+    asyncio.run(app.insert())
     print(app_id)
 
 
